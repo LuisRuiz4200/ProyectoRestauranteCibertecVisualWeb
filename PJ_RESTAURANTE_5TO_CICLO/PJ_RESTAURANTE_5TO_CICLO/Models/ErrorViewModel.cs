9826695261dec93e1d4a67a7f2a9bd092c21a7b7
LuisRuiz4200 <lr27072001@gmail.com>
2023-07-26 00:38:27 -0500

    Primer commit

---
+++ PJ_RESTAURANTE_5TO_CICLO/PJ_RESTAURANTE_5TO_CICLO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PJ_RESTAURANTE_5TO_CICLO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
