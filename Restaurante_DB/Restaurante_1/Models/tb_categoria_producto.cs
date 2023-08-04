fef7676a63f7053926f13bcc92872fbc251f8307
sadohu <sadohu26@yahoo.com>
2023-08-03 21:54:02 -0500

    Plantillas de API y Cliente

---
+++ Restaurante_DB/Restaurante_1/Models/tb_categoria_producto.cs
@@ -0,0 +1,8 @@
+﻿namespace Restaurante_1.Models
+{
+	public class tb_categoria_producto
+	{
+		public int id_categoria_producto { get; set; }
+		public string? des_categoria_producto { get; set; }
+	}
+}
